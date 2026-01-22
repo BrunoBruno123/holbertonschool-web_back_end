376d4ea8f61c1cc591c705f56be5f3052589dd1a
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-01-22 10:48:55 +0100

    changes

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 export default function handleResponseFromAPI(promise) {
     return promise
-    .then(() => {
-        return {status: 200, body: "success"};
-    })
-    .catch((err) => {
-        return new Error();
-    })
-    .finally(() => {
-        console.log("got a response form the api");
-});
+        .then(() => {
+            return { status: 200, body: "success" };
+        })
+        .catch((err) => {
+            return new Error(err);
+        })
+        .finally(() => {
+            console.log("Got a response from the API");
+        });
 }
