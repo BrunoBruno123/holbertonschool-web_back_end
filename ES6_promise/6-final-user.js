6bdab13a07537b5a8561bf5ef555b6033552f5dc
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-01-22 11:39:30 +0100

    changes

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,13 +6,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     const uploadPromise = uploadPhoto(fileName);
 
     return Promise.allSettled([signUpPromise, uploadPromise])
-        .then(results => {
-            return results.map(result => {
-                if (result.status === 'fulfilled') {
-                    return { status: result.status, value: result.value };
-                } else {
-                    return { status: result.status, value: result.reason };
-                }
-            });
-        });
+        .then(results => 
+            results.map(result => ({
+                status: result.status,
+                value: result.status === 'fulfilled' 
+                    ? result.value 
+                    : result.reason.toString()
+            }))
+        );
 }
