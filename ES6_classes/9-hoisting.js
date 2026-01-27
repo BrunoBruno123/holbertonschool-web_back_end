4563ab9fb6fb2500727c481eada6eee772817b4b
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-01-27 10:33:14 +0100

    changes

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -29,7 +29,7 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this._lastName} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.location}`;
   }
 }
 
