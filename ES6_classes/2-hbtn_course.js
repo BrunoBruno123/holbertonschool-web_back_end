7b178c266f84921622f72848012a6eae1e3943ce
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-01-27 09:15:30 +0100

    changes

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,52 @@
+class HolbertonCourse {
+    constructor(name, length, students) {
+        if (typeof name !== 'string') {
+            throw new Error('Name must be a string');
+        } else{
+            this._name = name;
+        }
+        if (typeof length !== 'number') {
+            throw new Error('Length must be a number');
+        } else {
+            this._length = length;
+        }
+        if (!Array.isArray(students) ) {
+            throw new Error('Students must be an array of strings');
+        } else {
+            this._students = students;
+        }
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+    get name() {
+        return this._name;
+    }
+    get length() {
+        return this._length;
+    }
+    get students() {
+        return this._students;
+    }
+    set name (newName) {
+        if (typeof newName !== 'string') {
+            throw new Error('Name must be a string');
+        } else {
+            this._name = newName;
+        }
+    }
+    set length (newLength) {
+        if (typeof newLength !== 'number') {
+            throw new Error('Length must be a number');
+        } else {
+            this._length = newLength;
+        }
+    }
+    set students (newStudents) {
+        if (!Array.isArray(newStudents)) {
+            throw new Error('Students must be an array of strings');
+        } else {
+            this._students = newStudents;
+        }
+  }
+}
