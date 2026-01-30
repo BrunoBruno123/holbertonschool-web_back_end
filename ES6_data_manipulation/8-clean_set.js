3d217c14402864e649256a972933d48f2d04b015
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-01-30 23:23:44 +0100

    changes

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
-export default function cleanSet(mySet, startString) {
-    if (!(mySet instanceof Set)) return '';
-
-    const filtered = Array.from(mySet)
-        .filter(value => typeof value === 'string' && value.startsWith(startString))
-        .map(value => value.slice(startString.length));
-
-    return filtered.join('-');
+export default function cleanSet(set, string) {
+    if(!string || string.length === 0){
+        return "";
+    }
+    const values = [];
+    for(const x of set.keys()){
+        if (x.includes(string)){
+            values.push(x.replace(string, ""));
+        }
+    }
+    return values.join("-");
 }
---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceries_list() {
+  return [
+    { item: 'Apples', quantity: 5 },
+    { item: 'Bananas', quantity: 3 },
+    { item: 'Carrots', quantity: 4 },
+    { item: 'Dates', quantity: 2 },
+    { item: 'Eggplants', quantity: 1 },
+  ];
+}
