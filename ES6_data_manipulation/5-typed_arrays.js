46400b0495f348aeca74398b7b868c6c02086250
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-01-30 14:07:20 +0100

    changes

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const typedArray = new Int8Array(length);
-  typedArray[position] = value;
-  return typedArray;
+  const buffer = new ArrayBuffer(length);
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+
+  return view;
 }
